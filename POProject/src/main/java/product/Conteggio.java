6211f05d0df3b5963a2df6e41ac135bf155db43c
michelepasqualini <48391278+michelepasqualini@users.noreply.github.com>
2019-07-05 10:14:13 +0200

    class conteggio

---
+++ POProject/src/main/java/product/Conteggio.java
@@ -0,0 +1,21 @@
+package product;
+
+public class Conteggio {
+	
+	private String element;
+	private int count;
+	
+	public Conteggio(String element, int count){
+		this.element = element;
+		this.count = count;
+	}
+
+	public String getElement() {
+		return element;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+}
