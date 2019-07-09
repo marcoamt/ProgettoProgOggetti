ee6c7bc85d648c1a1169d061d9169c58135a5508
michelepasqualini <48391278+michelepasqualini@users.noreply.github.com>
2019-07-09 12:13:35 +0200

    metadati javadoc

--- POProject/src/main/java/product/Metadati.java
+++ POProject/src/main/java/product/Metadati.java
@@ -6,20 +6,38 @@ public class Metadati {
 	private String sourceFields;
 	private String type;
 	
-	public Metadati(String alias, String sourceFields, String string) {
+	/**
+	 * 
+	 * @param alias is the name of field
+	 * @param sourceFields is the description of alias
+	 * @param type is the type of field
+	 */
+	public Metadati(String alias, String sourceFields, String type) {
 		this.alias = alias;
 		this.sourceFields = sourceFields;
-		this.type = string;
+		this.type = type;
 	}
-
+	
+	/**
+	 * 
+	 * @return the name of field
+	 */
 	public String getAlias() {
 		return alias;
 	}
 
+	/**
+	 * 
+	 * @return the description of field
+	 */
 	public String getSourceFields() {
 		return sourceFields;
 	}
 
+	/**
+	 * 
+	 * @return the type of field
+	 */
 	public String getType() {
 		return type;
 	}
