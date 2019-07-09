59f84488655333746777b2ab549e31742c0d9d4b
michelepasqualini <48391278+michelepasqualini@users.noreply.github.com>
2019-07-09 12:14:53 +0200

    controller javadoc

--- POProject/src/main/java/product/ProdottiController.java
+++ POProject/src/main/java/product/ProdottiController.java
@@ -23,47 +23,58 @@ public class ProdottiController {
         return "home";
     }
 	
+	/**
+	 * This method prints out a list of all type of metadata
+	 * @return a list of all type of metadata
+	 */
 	@GetMapping("/metadata")
 	public List<Metadati> getAllMeta() {
 		return prodottiService.getAllMeta();
 	}
 	
+	/**
+	 * This method prints out a list of all products
+	 * @return  a list of all products
+	 */
 	@GetMapping("/prodotti")
 	public List<Prodotti> prodottiFilterPrice(){
-		
 			return prodottiService.getAllProducts();
-	
-		
 	}
 	
-	//estrae prodotti con codice = filter
+	/**
+	 * This method prints out a list of products with code = filter
+	 * @return a list of products with code = filter
+	 */
 	@GetMapping("/prodotti/{filter}")
 	public List<Prodotti> getPByCode(@PathVariable int filter){
 		return prodottiService.getProductByCode(filter);
 	}
 	
-	/*@GetMapping("/prodotti")
-	public List<String> getProductByFilter(@RequestParam(value="filter", required=true) String filter){
-		return prodottiService.getProdByFilter(filter);
-	}*/
-	
-	
-	//estrae il conteggio di ogni tipo di prodotto 
+	/**
+	 * This method prints out a list of count for each type of product
+	 * @return a list of count for each type of product
+	 */
 	@GetMapping("/statstring")
 	public List<Conteggio> getCountElem(@RequestParam(value = "filter", required=false)String filter,@RequestParam String field){
-		
+
 			return prodottiService.getCountElement(field,filter);		
 	}
-	
+	/**
+	 * This method prints out a list of products filtered with code
+	 * @return a list of products filtered with code
+	 */
 	@PostMapping(path="/prodotti", consumes= {"application/JSON"})
 	public List<Prodotti> getPByCode(@RequestBody String filter){
 		return prodottiService.getProductByCodeFiltro(filter);
 	}
 	
+	/**
+	 * This method prints out statistics all data
+	 * @return statistics all data
+	 */
 	@GetMapping("/stats")
 	public Item getStats(@RequestParam(value = "filter", required=false)String filter,@RequestParam String field)  {
-		
-			return prodottiService.getStatsFiltro(filter, field);
+		return prodottiService.getStatsFiltro(filter, field);
 	}
 	
 	
